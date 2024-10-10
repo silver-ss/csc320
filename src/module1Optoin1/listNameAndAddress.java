45f69eb1cc0fef6e535508e7edca84dcb100cf80
Stephen <smsavell@gmail.com>
2024-10-09 22:05:26 -0600

    initial commit for assignment 1

---
+++ src/module1Optoin1/listNameAndAddress.java
@@ -0,0 +1,29 @@
+package module1Optoin1;
+
+import java.util.Scanner;
+
+public class listNameAndAddress {
+	
+	public static void main(String[] args) {
+		String firstName;
+		String lastName;
+		String streetAddress;
+		String city;
+		int zipCode;
+		
+		firstName = "Harry";
+		lastName = "Potter";
+		streetAddress = "1000 Hogwarts Drive";
+		city = "Hogwarts";
+		zipCode = 12345;
+				
+		System.out.println("Field		|Value");
+		System.out.println("----------------------");
+		System.out.println("First Name:	|" + firstName);
+		System.out.println("Last Name: 	|" + lastName);
+		System.out.println("Address: 	|" + streetAddress);
+		System.out.println("City: 		|" + city);
+		System.out.println("Zip Code: 	|" + firstName);
+	}
+
+}
