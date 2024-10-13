ec9c1f9ee2d486be2ba77d82f6825abb49fd30bb
Stephen <smsavell@gmail.com>
2024-10-13 14:51:11 -0600

    adding comments

--- src/module1Optoin1/listNameAndAddress.java
+++ src/module1Optoin1/listNameAndAddress.java
@@ -3,18 +3,21 @@ package module1Optoin1;
 public class listNameAndAddress {
 	
 	public static void main(String[] args) {
+		// Declare the 5 variables
 		String firstName;
 		String lastName;
 		String streetAddress;
 		String city;
 		int zipCode;
 		
+		// Set the values of the variables
 		firstName = "Harry";
 		lastName = "Potter";
 		streetAddress = "1000 Hogwarts Drive";
 		city = "Hogwarts";
 		zipCode = 12345;
 				
+		// Print out a table of the readable names of each variable and the values associated
 		System.out.println("Field		|Value");
 		System.out.println("----------------------");
 		System.out.println("First Name:	|" + firstName);
