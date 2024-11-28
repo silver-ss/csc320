dd463592e16759e4143db7960c52370276445728
Stephen <smsavell@gmail.com>
2024-11-28 16:11:08 -0700

    updates to code

--- assignment4.java
+++ assignment4.java
@@ -1,42 +1,64 @@
 package cta4;
 
+import java.util.InputMismatchException;
 import java.util.Scanner; 
 
 public class CriticalThinkingAssignmentOption1 {
 	public static void main(String[] args) {
-		float total = 0.0f;
 		int count = 0; 
-		float max = Float.MIN_VALUE;
-		float min = Float.MAX_VALUE;
 		float interestRate = 0.2f;
-		float userInput;
-		float average;
-		float interest;
+		float total;
 		Scanner scnr = new Scanner(System.in);
+		float [] array = new float[5];
 
-		
 		while (count < 5) {
 			System.out.println("Enter a floating point number " + (count + 1) + " of 5:");
-			userInput = scnr.nextFloat();
-			//add new number to total
-			total += userInput;
-			//update max is new number is the greatest
-			if(userInput > max) {
-				max = userInput;
-			}
-			//update min if new number is the smallest
-			if(userInput < min) {
-				min = userInput;
-			}
+			try{
+			array[(count)] = scnr.nextFloat();
 			++count;
+			} catch (InputMismatchException e) { // present error if non-float is provided
+				System.out.println("ERROR: Invalid entry provided, pleae provide a decimal number like 2.4");
+				scnr.nextLine(); // clear scanner
+			}
 		}
-		average = total/count;
-		interest = total * interestRate;
-		
-		System.out.println("The Total is: " + total);
-		System.out.println("The Average is: " + average);
-		System.out.println("The Maximum is: " + max);
+		scnr.close();
+		//call methods to print out results
+		total = getTotal(array);
+		getAverage(total, count);
+		getMax(array);
+		getMin(array);
+		getInterest(interestRate, total);	
+	}
+	// find minimum values in array
+	private static void getMin(float [] array) {
+		float min = array[0];
+		for (float num : array) {
+			if (num < min) {min = num;}
+		}
 		System.out.println("The Minimum is: " + min);
-		System.out.println("The Interest at " + (interestRate * 100) + "% is: " + interest);	
+	}
+	// find maximum values in array
+	private static void getMax(float [] array) {
+		float max = array[0];
+		for (float num : array) {
+			if (num > max) {max = num;}
+		}
+		System.out.println("The Maximum is: " + max);
+	}
+	private static float getTotal(float [] array) {
+		float total = 0.0f;
+		for (float num : array) {
+			total += num;
+		}
+		System.out.println("The Total is: " + total);
+		return total;
+	}
+	private static void getAverage (float total, float count) {
+		float average = (total / count);
+		System.out.printf("The Average is: %.2f\n", average);
+	}
+	private static void getInterest(float interestRate, float total) {
+		float interest = total * interestRate;
+		System.out.printf("The Interest at %.1f%% is %.2f\n", (interestRate * 100), interest);
 	}
 }
