f28bf0a5a0d12617a9f45d61d2f6c90f254fa994
Stephen <smsavell@gmail.com>
2024-11-28 17:29:01 -0700

    updating code

--- assignment5Option1.java
+++ assignment5Option1.java
@@ -5,37 +5,27 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class weeklyTemps {	
+public class WeeklyTemps {	
 	private static ArrayList<String> dayOfWeek = new ArrayList<>(Arrays.asList(
 			"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",	"Saturday", "Sunday"));
 	private static ArrayList<Double> dailyTemp = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		System.out.println("Provide the past 7 average daily tempatures seperated by spaces");
-		
-		
-		while (dailyTemp.size() < 7) {
-			try {
-				dailyTemp.add(scnr.nextDouble());
-			} catch (InputMismatchException e) {
-				System.out.println("Please enter a valid number");
-				scnr.next(); // clear invalid inputs before new input
-			}
-		}
-				
 		boolean continueWhile = true;
+		
+		setTemps(dayOfWeek, scnr);
+		
 		while(continueWhile) {
 			menuPrompt();
 			String userEntry = scnr.next();
-		
 			switch (userEntry.toLowerCase()) {
 				case "quit":
 					continueWhile = false;
 					System.out.println("Ending in 3...2..1");
 					break;
 				case "week":
-					getWeek(userEntry);
+					getWeek();
 					break;
 				default:
 					getDayTemp(userEntry);
@@ -47,7 +37,7 @@ public class weeklyTemps {
 		
 	// initial prompt 
 	public static void menuPrompt() {
-		System.out.println("Enter a day of the week such as 'Monday' to see daily average tempature");
+		System.out.println("\nEnter a day of the week such as 'Monday' to see daily average tempature");
 		System.out.println("Enter 'week' to see daily average tempatures for the full week");
 		System.out.println("Enter 'quit' to exit");
 	}
@@ -68,12 +58,27 @@ public class weeklyTemps {
 	}
 	
 	// print days and weekly average
-	public static void getWeek(String week) {
+	public static void getWeek() {
 		double sum = 0.0;		
 		for (int i = 0; i < dayOfWeek.size(); ++i) {
 			System.out.println("On " + dayOfWeek.get(i) + " it was " + dailyTemp.get(i) + " degrees.");
 			sum += dailyTemp.get(i);
 		}
-		System.out.println("The weekly average tempature was: " + (sum/dayOfWeek.size()) + " degrees" );
+		System.out.printf("The weekly average tempature was: %.2f degrees\n",  (sum/dayOfWeek.size()));
+	}
+	
+	// prompt user and collect daily average temperatures
+	public static void setTemps (ArrayList<String> dayOfWeek, Scanner scnr) {
+		int i = 0;
+		while (i < dayOfWeek.size()) {
+			try {
+				System.out.printf("Provide the average temperature for last %s:\n", dayOfWeek.get(i));
+				dailyTemp.add(scnr.nextDouble());
+				++i;
+			} catch (InputMismatchException e) {
+				System.out.println("ERROR: Invalid value provided Please proivde the temperature for " + dayOfWeek.get(i));
+				scnr.nextLine(); // clear scanner
+			}
+		}
 	}
 }
